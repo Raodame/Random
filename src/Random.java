404f32950163352846f2f284993b1b33fde3e98f
Asher Kim <akim3213@stu.d214.org>
2021-12-14 12:21:25 -0600

    actual

--- src/Random.java
+++ src/Random.java
@@ -2,15 +2,13 @@ public class Random {
 
     public static int nextInt(int r){
         int x;
-        if (r >= 0) {
+        if (r < 0)
+            System.exit(10);
             do {
                 x = (int) (Math.random() * r);
             }
             while (x == r);
             return x;
-        }
-        else
-            return Integer.parseInt("System.exit(10);");
 
     }
 
--- src/RandomRunner.java
+++ src/RandomRunner.java
@@ -3,7 +3,7 @@
 public class RandomRunner {
 	public static void main(String args[]){
 		Random r = new Random();
-		System.out.println("Integer=" + r.nextInt(5) );
+		System.out.println("Integer=" + r.nextInt(10) );
 		System.out.println("Double=" + r.nextDouble() );
 		System.out.println("Boolean=" + r.nextBoolean() );
 	}
